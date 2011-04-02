61314604f6c803e98f9a7fb8154ebce0e744e784
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2011-04-03 02:34:04 +0700

    link

--- Templates/footer.php
+++ Templates/footer.php
@@ -1,3 +1,3 @@
 
-Powered by dtdocs :: <a href="http://validator.w3.org/check?uri=referer">valid html5</a>
+Powered by <a href="https://github.com/dtinth/dtdocs">dtdocs</a> :: <a href="http://validator.w3.org/check?uri=referer">valid html5</a>
 
